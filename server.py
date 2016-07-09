b12a0fec30860143232d54314f0a7b1bf9c11898
Advith Chelikani <advith.chelikani@gmail.com>
2016-07-09 16:58:25 -0700

    code golf

--- server.py
+++ server.py
@@ -1,6 +1,5 @@
 from flask import Flask, flash, redirect, render_template, request, session, abort, url_for
 
-
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
@@ -18,6 +17,21 @@ def game():
     if name is None:
         name = "gamesgamesgames"
     return render_template('games.html')
+
+@app.route('/api/code_golf')
+def golf(username=None):
+    user_code = request.args.get('code')
+    question = "Check if a given positive integer is divisible by 2."
+    #test_cases = {0 : True, 5 : False, 4: True, 1024 : True
+    test_case = 5
+    try:
+
+        print eval(user_code[1:-1])
+        if eval(user_code) == test_case:
+            return "success"
+        return "failure - wrong answer"
+    except:
+        return "failure - invalid eval"
 
 @app.route('/play/<gamename>')
 def play_game(gamename, username=None):
