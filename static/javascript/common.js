0831ed4b3190010ac30f05afaf2315da7db9014b
Chen <riichenchen@gmail.com>
2016-07-09 16:47:39 -0700

    Fixing leaderboard + starting reorganization

---
+++ static/javascript/common.js
@@ -0,0 +1,10 @@
+var config = {
+  apiKey: "AIzaSyDq5GXyq96eg0FsUOfVM4XhH1cBS57m4C8",
+  authDomain: "minigames-15a0f.firebaseapp.com",
+  databaseURL: "https://minigames-15a0f.firebaseio.com",
+  storageBucket: "minigames-15a0f.appspot.com",
+};
+
+function start_firebase() {
+    firebase.initializeApp(config);
+}
