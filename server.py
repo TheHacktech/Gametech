725e7a7434d89e204df486ae6c337c9a2dc5bb5d
Advith Chelikani <advith.chelikani@gmail.com>
2016-07-09 14:31:00 -0700

    routes to leaderboard

--- server.py
+++ server.py
@@ -8,17 +8,16 @@ app.config['DEBUG'] = True
 def index():
     return 'Index Page'
 
+@app.route('/leaderboard')
+def leaderboard(name=None):
+    name = request.args["name"]
+    return render_template('leaderboard.html',name=name)
+
 @app.route('/games')
 def game():
     if name is None:
         name = "gamesgamesgames"
     return render_template('games.html')
-
-@app.route('/hello')
-def hello(name=None):
-    if name is None:
-        name = "Chenchenchenchenchen"
-    return render_template('hello.html', name=name)
 
 @app.route('/play/<gamename>')
 def play_game(gamename, username=None):
@@ -27,14 +26,14 @@ def play_game(gamename, username=None):
         return redirect(url_for('login'))
     return render_template('games/%s.html' % gamename)
 
-@app.route('/log', methods=['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
     if request.method == 'POST':
         if request.form['username'] != 'admin' or request.form['password'] != 'admin':
             error = 'Invalid Credentials. Please try again.'
         else:
-            return redirect(url_for('hello'))
+            return redirect(url_for('leaderboard', name=request.form['username']))
     return render_template('login.html', error=error)
 
 if __name__ == "__main__":
