009717fe1bf50fb5189f026644ed293b3e02b35a
Advith Chelikani <advith.chelikani@gmail.com>
2016-07-09 14:17:27 -0700

    resolving conflicts
    Resolved conflcits

---
+++ static/js/leaderboard.js
@@ -0,0 +1,7 @@
+var config = {
+  apiKey: "AIzaSyDq5GXyq96eg0FsUOfVM4XhH1cBS57m4C8",
+  authDomain: "minigames-15a0f.firebaseapp.com",
+  databaseURL: "https://minigames-15a0f.firebaseio.com",
+  storageBucket: "",
+};
+firebase.initializeApp(config);
